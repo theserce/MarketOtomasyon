4f9c98c9f25167fea54d4785ec41574598ffdc64
muhammettalhakaymak23214 <talha.kaymak@bil.omu.edu.tr>
2023-01-05 15:36:25 +0300

    Borç sayfası yapıldı.

--- MarketOtomasyon/UserControls/borc.Designer.cs
+++ MarketOtomasyon/UserControls/borc.Designer.cs
@@ -28,18 +28,243 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(borc));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.silBtn = new System.Windows.Forms.Button();
+            this.kaydetBtn = new System.Windows.Forms.Button();
+            this.guncelleBtn = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.dataGridView2);
+            this.panel1.Controls.Add(this.silBtn);
+            this.panel1.Controls.Add(this.kaydetBtn);
+            this.panel1.Controls.Add(this.guncelleBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(848, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(363, 546);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(148, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 27);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Toplam Borç Miktarı";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column7});
+            this.dataGridView2.Location = new System.Drawing.Point(118, 215);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 29;
+            this.dataGridView2.Size = new System.Drawing.Size(194, 77);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // silBtn
+            // 
+            this.silBtn.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.silBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.silBtn.Image = ((System.Drawing.Image)(resources.GetObject("silBtn.Image")));
+            this.silBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.silBtn.Location = new System.Drawing.Point(235, 416);
+            this.silBtn.Name = "silBtn";
+            this.silBtn.Size = new System.Drawing.Size(100, 42);
+            this.silBtn.TabIndex = 2;
+            this.silBtn.Text = "Sil";
+            this.silBtn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.silBtn.UseVisualStyleBackColor = false;
+            this.silBtn.MouseEnter += new System.EventHandler(this.silBtn_MouseEnter);
+            this.silBtn.MouseLeave += new System.EventHandler(this.silBtn_MouseLeave);
+            // 
+            // kaydetBtn
+            // 
+            this.kaydetBtn.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.kaydetBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kaydetBtn.Image = ((System.Drawing.Image)(resources.GetObject("kaydetBtn.Image")));
+            this.kaydetBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.kaydetBtn.Location = new System.Drawing.Point(235, 358);
+            this.kaydetBtn.Name = "kaydetBtn";
+            this.kaydetBtn.Size = new System.Drawing.Size(100, 42);
+            this.kaydetBtn.TabIndex = 1;
+            this.kaydetBtn.Text = "Kaydet";
+            this.kaydetBtn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.kaydetBtn.UseVisualStyleBackColor = false;
+            this.kaydetBtn.MouseEnter += new System.EventHandler(this.kaydetBtn_MouseEnter);
+            this.kaydetBtn.MouseLeave += new System.EventHandler(this.kaydetBtn_MouseLeave);
+            // 
+            // guncelleBtn
+            // 
+            this.guncelleBtn.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.guncelleBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.guncelleBtn.Image = ((System.Drawing.Image)(resources.GetObject("guncelleBtn.Image")));
+            this.guncelleBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.guncelleBtn.Location = new System.Drawing.Point(235, 472);
+            this.guncelleBtn.Name = "guncelleBtn";
+            this.guncelleBtn.Size = new System.Drawing.Size(100, 42);
+            this.guncelleBtn.TabIndex = 0;
+            this.guncelleBtn.Text = "Güncelle";
+            this.guncelleBtn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.guncelleBtn.UseVisualStyleBackColor = false;
+            this.guncelleBtn.MouseEnter += new System.EventHandler(this.guncelleBtn_MouseEnter);
+            this.guncelleBtn.MouseLeave += new System.EventHandler(this.guncelleBtn_MouseLeave);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(848, 546);
+            this.panel2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(848, 546);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 125;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 125;
             // 
             // borc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "borc";
             this.Size = new System.Drawing.Size(1211, 546);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Panel panel1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label1;
+        private DataGridView dataGridView2;
+        private Button silBtn;
+        private Button kaydetBtn;
+        private Button guncelleBtn;
+        private Panel panel2;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column6;
+        private DataGridViewTextBoxColumn Column7;
     }
 }
